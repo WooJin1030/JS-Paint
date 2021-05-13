359ffb4b1470314a73e5e71c7843e81b60a75ed5
Woojin <sam4woojin@naver.com>
2021-05-13 10:55:47 +0900

    Add Mode Fill

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.querySelectorAll(".jsColor");
 const range = document.querySelector("#jsRange");
+const mode = document.querySelector("#jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITAL_COLOR;
+ctx.fillStyle = INITAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 const onMouseMove = (e) => {
   const x = e.offsetX;
@@ -34,11 +40,29 @@ const startPainting = () => {
 const handleColorClick = (e) => {
   const color = e.target.style.backgroundColor;
   ctx.strokeStyle = color;
+
+  ctx.fillStyle = color;
 };
 
 const handleRangeChange = (e) => {
   const size = e.target.value;
   ctx.lineWidth = size;
+};
+
+const handleModeClick = (e) => {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+};
+
+const handleCanvasClick = () => {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
 };
 
 if (canvas) {
@@ -47,8 +71,13 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   colors.forEach((color) => color.addEventListener("click", handleColorClick));
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 if (range) {
   range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
 }
